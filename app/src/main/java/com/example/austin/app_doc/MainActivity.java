9e6ef83bdf4fb49ab45dda67c4d878cf32efe5ae
AgustinMTZ <power_gugus52@hotmail.com>
2018-11-13 23:47:17 -0600

    se creo la interfaz del login View 1 con animacion

--- app/src/main/java/com/example/austin/app_doc/MainActivity.java
+++ app/src/main/java/com/example/austin/app_doc/MainActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //OCULTAR ACTIONBAR
         getSupportActionBar().hide();
+        imageView = (ImageView) findViewById(R.id.image1);
+        imageView.animate().scaleX(2).scaleY(2).setDuration(5000).start();
     }
 }
